82156afe8f5542b0af06db169e8ba5e9edb972a7
ScheppertoO <ke.schmidt1993@gmail.com>
2025-03-30 23:41:53 +0200

    No code changes made

--- AddUsersWithOUstrukture.ps1
+++ AddUsersWithOUstrukture.ps1
@@ -24,7 +24,7 @@ $ouStructure = @(
 )
 
 foreach ($ou in $ouStructure) {
-    if (-not (Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$($ou)'")) {
+    if (-not (Get-ADOrganizationalUnit -Filter {DistinguishedName -eq $ou})) {
         $ouName = $ou.Split(',')[0] -replace 'OU=', ''
         New-ADOrganizationalUnit -Name $ouName -Path ($ou -replace '^OU=.*?,', '')
     }
@@ -35,81 +35,6 @@ New-ADOrganizationalUnit -Name Clients -Path "OU=Technotrans,DC=Technotrans,DC=d
 New-ADOrganizationalUnit -Name GL-Gruppen -Path "OU=Gruppen,OU=Technotrans,DC=Technotrans,DC=dom"
 New-ADOrganizationalUnit -Name DL-Gruppen -Path "OU=Gruppen,OU=Technotrans,DC=Technotrans,DC=dom"
 
-<# Alter Weg 
-foreach ($user in $users) {
-    # Benutzer erstellen, falls nicht bereits vorhanden
-    if (-not (Get-ADUser -Filter "Name -eq '$($user.Name)'")) {
-        New-ADUser -Name $user.Name -Department $user.Department -Path $user.OU -AccountPassword (ConvertTo-SecureString "Passwort123!" -AsPlainText -Force) -Enabled $true
-    }
-    
-    # ueberpruefen, ob die Abteilung eine entsprechende Gruppe hat
-    if ($departmentGroupMapping.ContainsKey($user.Department)) {
-        $groupName = $departmentGroupMapping[$user.Department]
-
-        # ueberpruefen, ob die Gruppe existiert, und erstellen, falls nicht
-        if (-not (Get-ADGroup -Filter "Name -eq '$groupName'")) {
-            New-ADGroup -Name $groupName -GroupScope Global -Path "OU=Groups,DC=Technotrans,DC=dom"
-        }
-
-        # Benutzer der Gruppe hinzufuegen
-        Add-ADGroupMember -Identity $groupName -Members $user.Name
-    }
-}
-
-
-# Globale Gruppen erstellen
-$globalGroups = @("GL-Buchhaltung-Group", "GL-Marketing-Group", "GL-IT-Group")
-foreach ($group in $globalGroups) {
-    Write-Host "Pruefe Gruppe: $group"
-    if (-not (Get-ADGroup -Filter {Name -eq $group})) {
-        Write-Host "Erstelle Gruppe: $group"
-        New-ADGroup -Name $group -GroupScope Global -Path "DC=demo,DC=dom"
-    } else {
-        Write-Host "Gruppe existiert bereits: $group"
-    }
-}
-
-# Benutzer anlegen und zu globalen Gruppen hinzufuegen
-foreach ($user in $users) {
-    $username = $user.Name -replace " ", "."
-    $password = "Password1"  # Sicherste Passwort der Welt
-    $ou = $user.OU
-    $userPrincipalName = "$username@demo.dom"
-
-    Write-Host "Erstelle Benutzer: $user.Name"
-    New-ADUser -Name $user.Name -GivenName $user.Name.Split(" ")[0] -Surname $user.Name.Split(" ")[1] `
-               -SamAccountName $username -UserPrincipalName $userPrincipalName `
-               -Path $ou -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -Enabled $true -PasswordNeverExpires $true
-
-    # Benutzer zu globalen Gruppen hinzufuegen
-    Write-Host "Fuege Benutzer $username zu Gruppe $($user.Department)-Group hinzu"
-    Add-ADGroupMember -Identity "$($user.Department)-Group" -Members $username
-}
-
-# Lokale Domaenengruppen erstellen und globale Gruppen hinzufuegen
-$domainLocalGroups = @("DL-Buchhaltung-Daten", "DL-Marketing-Daten", "DL-IT-Daten")
-foreach ($group in $domainLocalGroups) {
-    Write-Host "Pruefe lokale Domaenengruppe: $group"
-    if (-not (Get-ADGroup -Filter {Name -eq $group})) {
-        Write-Host "Erstelle lokale Domaenengruppe: $group"
-        New-ADGroup -Name $group -GroupScope DomainLocal -Path "DC=demo,DC=dom"
-    } else {
-        Write-Host "Lokale Domaenengruppe existiert bereits: $group"
-    }
-
-    # Globale Gruppe zu lokaler Domaenengruppe hinzufuegen
-    if ($group -eq "DL-Buchhaltung-Daten") {
-        Write-Host "Fuege GL-Buchhaltung-Group zu $group hinzu"
-        Add-ADGroupMember -Identity $group -Members "GL-Buchhaltung-Group"
-    } elseif ($group -eq "DL-Marketing-Daten") {
-        Write-Host "Fuege GL-Marketing-Group zu $group hinzu"
-        Add-ADGroupMember -Identity $group -Members "GL-Marketing-Group"
-    } elseif ($group -eq "DL-IT-Daten") {
-        Write-Host "Fuege GL-IT-Group zu $group hinzu"
-        Add-ADGroupMember -Identity $group -Members "GL-IT-Group"
-    }
-}
-    #>
 # Benutzerberechtigungen definieren
 $userPermissions = @(
     @{Name="Ute Unten"; Permissions=@("DL-Versand-Read", "DL-Versand-Write")},
@@ -118,18 +43,18 @@ $userPermissions = @(
 )
 
 # Lokale Domaenengruppen erstellen (nur Read, Write, Full pro Abteilung)
-$departments = @("Buchhaltung", "Marketing", "IT", "Versand", "Vertrieb", "Geschaeftsfuehrung")
+$departments = @("Buchhaltung", "Marketing", "IT", "Versand-Abt", "Vertrieb-Abt", "Gefue-Abt")
 foreach ($department in $departments) {
     foreach ($suffix in @("Read", "Write", "Full")) {
         $groupName = "DL-$department-$suffix"
         Write-Host "Pruefe lokale Domaenengruppe: $groupName"
         if (-not (Get-ADGroup -Filter {Name -eq $groupName})) {
             Write-Host "Erstelle lokale Domaenengruppe: $groupName"
-            New-ADGroup -Name $groupName -GroupScope DomainLocal -Path "DC=technotrans,DC=dom"
+            New-ADGroup -Name $groupName -GroupScope DomainLocal -GroupCategory Security -Path "DC=technotrans,DC=dom"
         } else {
             Write-Host "Lokale Domaenengruppe existiert bereits: $groupName"
         }
-    }
+   }
 }
 
 # Benutzer anlegen und zu den entsprechenden Gruppen hinzufuegen
@@ -139,21 +64,37 @@ foreach ($user in $users) {
     $ou = $user.OU
     $userPrincipalName = "$username@technostrans.dom"
 
-    Write-Host "Erstelle Benutzer: $user.Name"
-    New-ADUser -Name $user.Name -GivenName $user.Name.Split(" ")[0] -Surname $user.Name.Split(" ")[1] `
-               -SamAccountName $username -UserPrincipalName $userPrincipalName `
-               -Path $ou -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -Enabled $true -PasswordNeverExpires $true
+    # Erstelle den Benutzer nur, falls er noch nicht existiert
+    if (-not (Get-ADUser -Filter {SamAccountName -eq $username})) {
+        Write-Host "Erstelle Benutzer: $($user.Name)"
+        New-ADUser -Name $user.Name -GivenName $user.Name.Split(" ")[0] -Surname $user.Name.Split(" ")[1] `
+                   -SamAccountName $username -UserPrincipalName $userPrincipalName `
+                   -Path $ou -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -Enabled $true -PasswordNeverExpires $true
+    } else {
+        Write-Host "Benutzer $username existiert bereits."
+    }
 
-    # Benutzer zu globalen Gruppen hinzufuegen
-    Write-Host "Fuege Benutzer $username zu Gruppe $($user.Department)-Group hinzu"
-    Add-ADGroupMember -Identity "$($user.Department)-Group" -Members $username
+    # Sicherstellen, dass die Abteilungsgruppe existiert, z.B. "Versand-Group"
+    $deptGroup = "$($user.Department)-Group"
+    if (-not (Get-ADGroup -Filter {Name -eq $deptGroup})) {
+         Write-Host "Erstelle Gruppe: $deptGroup"
+         New-ADGroup -Name $deptGroup -GroupScope Global -Path "OU=Gruppen,OU=Technotrans,DC=Technotrans,DC=dom"
+    }
+
+    # Benutzer der Abteilungsgruppe hinzufügen
+    Write-Host "Fuege Benutzer $username zu Gruppe $deptGroup hinzu"
+    Add-ADGroupMember -Identity $deptGroup -Members $username
 
     # Benutzerberechtigungen auslesen und zu den entsprechenden Gruppen hinzufuegen
     $userPermission = $userPermissions | Where-Object { $_.Name -eq $user.Name }
     if ($null -ne $userPermission) {
         foreach ($permissionGroup in $userPermission.Permissions) {
             Write-Host "Fuege Benutzer $username zu Gruppe $permissionGroup hinzu"
-            Add-ADGroupMember -Identity $permissionGroup -Members $username
+            if (Get-ADGroup -Filter {Name -eq $permissionGroup}) {
+                Add-ADGroupMember -Identity $permissionGroup -Members $username
+            } else {
+                Write-Host "Gruppe $permissionGroup existiert nicht. Ueberspringe Hinzufuegen."
+            }
         }
     } else {
         Write-Host "Keine spezifischen Berechtigungen fuer Benutzer $username gefunden."
